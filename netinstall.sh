421a48d76cc39b87ed99d6c85f153a49041eebd0
Adam Brenecki <adam@brenecki.id.au>
2012-01-28 14:08:50 +1030

    Added (somewhat dodgy) eclipse installer

--- netinstall.sh
+++ netinstall.sh
@@ -2,6 +2,11 @@ echo "Downloading eclipse-get..."
 mkdir eclipse-get
 wget -qO- https://github.com/adambrenecki/eclipse-get/tarball/master | tar zx -C eclipse-get/ --strip-components 1
 cd eclipse-get
+echo "Do you want to install Eclipse too? [y/n]"
+read run_eclipse_installer
+if [ $run_eclipse_installer == "y" ]; then
+	./install-eclipse.sh
+fi
 echo "Installing eclipse-get. You'll probably be asked for your root password."
 sudo make install
 cd ..
